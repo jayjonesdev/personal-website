456304c02e208d76738db93748ecec12372a0b2a
Jerrell <jayjonesdevelopment@gmail.com>
2026-07-23 10:04:02 -0500

    remove site link

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // NOTE: update `site` to your real domain before deploying — it's used for
 // canonical + Open Graph absolute URLs.
 export default defineConfig({
-  site: 'https://jerrelljones.dev',
+  site: 'https://jayjones.dev',
 });
